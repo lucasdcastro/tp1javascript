a4308ec183a9825b72cace7c1d0d5ae900bce2d8
Giovanna Anacleto <giovana.anacleto@al.infnet.edu.br>
2017-10-30 20:02:50 +0000

    Atualizando app.js, novas functions para verificar letra

--- app.js
+++ app.js
@@ -19,20 +19,63 @@ var forca = function(){
             this.lacunas += '_ ';
         }
         exibeHTML('palavra',this.lacunas);
-          document.onkeypress = function(evt) {
+          document.onkeypress = (evt) => {
              evt = evt || window.event;
-            var charCode = evt.keyCode || evt.which;
-             // var charStr = String.fromCharCode(charCode);
-             this.verificaLetra(charCode);
+             var charCode = evt.keyCode || evt.which;
+             var charStr = String.fromCharCode(charCode);
+             this.verificaLetra(charStr);
         };
         //window.document.getElementById('palavra').innerHTML = lacunas;
     };
+    var verificaLetra = function(c){
+        //variavel com o valor da palavra sorteada
+        var valor = palavraSorteada.indexOf(c);
+        
+        if(valor == -1){
+            adicionaErro(c);
+        }
+        else{
+            adicionaAcerto(c);
+        }
+        
+        atualizaLacuna(c);
+        
+        //se a letra na palavra sorteada
+        //adicionaacerto
+        //senao
+        //adicionaerro
+        console.log(c);
+    };
+    var adicionaAcerto = function(c){
+        var valor = acertos.indexOf(c);
+        if (valor == -1) {
+             adicionaNovoAcerto(c);
+        }
+      
+        
+        //se letra ja esta em acerto
+        //nao faz nada
+        //senao
+        //adicionaNovoAcerto
+    };
+    var adicionaErro = function(c){
+        var valor = erros.indexOf(c);
+        if(valor == -1){
+            adicionaNovoErro(c);
+        }
+        //adiciona letra ao atributo acertos
+    };
+    var adicionaNovoAcerto = function(c){
+        var acertos = acertos + c;  
+    };
+    var adicionaNovoErro = function(c){
+        var erros = erros + c;
+    };
+    var atualizaLacuna = function(){};
     var exibeHTML = function(a,b){
         document.getElementById(a).innerHTML = b;
     }
-    var verificaLetra = function(){
-        
-    };
+    
     return{
         lacunas : lacunas,
         erros : erros,
